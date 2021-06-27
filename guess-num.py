0084b4768beca3888b38616efd494dc5239b5544
Nick Liu <hybrida666@icloud.com>
2021-06-27 16:29:40 +0800

    final version

--- guess-num.py
+++ guess-num.py
@@ -1,6 +1,11 @@
 import random
+start = input('Please choose start from what number.')
+end = input('Please choose end at what number.')
+start = int(start)
+end = int(end)
 
-r = random.randint(1, 100)
+
+r = random.randint(start, end)
 count = 0
 while True:
 	count += 1 # count = count + 1
