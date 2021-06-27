2539a2f22258f36622e314457698e16ad75e3ee8
Nick Liu <hybrida666@icloud.com>
2021-06-27 16:25:22 +0800

    add counting

--- guess-num.py
+++ guess-num.py
@@ -1,13 +1,17 @@
 import random
 
 r = random.randint(1, 100)
+count = 0
 while True:
+	count += 1 # count = count + 1
 	num = input('Please guess the number.')
 	num = int(num)
 	if num == r:
 		print('You are correct!')
+		print('This is the', count, 'time you guessed.')
 		break
 	elif num > r:
 		print('Too big')	
 	elif num < r:
 		print('Too small')	
+	print('This is the', count, 'time you guessed.')	
